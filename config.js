5ac6235391f8064005fe90316f424d8fb5ede919
Ingar Almklov <ingara@gmail.com>
2018-11-29 10:25:21 +0100

    Update GA-tracking ID

--- config.js
+++ config.js
@@ -3,15 +3,15 @@
 // what not here.
 
 module.exports = {
-    name: 'elm.christmas',
-    domain: 'https://elm.christmas',
-    ogDescription: 'A nice little Christmas calendar for people who love Elm',
-    firstYearOfContent: 2018,
-    otherChristmasCalendars: [
-        'javascript.christmas',
-        'security.christmas',
-        'react.christmas',
-    ],
-    handleLegacyLinks: true,
-    googleAnalyticsTrackingId: 'UA-130120068-1',
+  name: 'elm.christmas',
+  domain: 'https://elm.christmas',
+  ogDescription: 'A nice little Christmas calendar for people who love Elm',
+  firstYearOfContent: 2018,
+  otherChristmasCalendars: [
+    'javascript.christmas',
+    'security.christmas',
+    'react.christmas'
+  ],
+  handleLegacyLinks: true,
+  googleAnalyticsTrackingId: 'UA-110431089-2'
 };
